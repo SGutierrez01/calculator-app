8ca0a756e6b73d129bd8ce88ed9c3fc3a14a9492
Santiago Gutierrez Villegas <santiago.gutierrez01@globant.com>
2024-08-09 15:38:13 -0500

    Feat: Create sum and subtract methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,12 @@
 public class Calculator {
     public static void main(String[] args){
     }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
