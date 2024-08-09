3019f95d026d17e68cff54a97800791bdd4f807f
Santiago Gutierrez Villegas <santiago.gutierrez01@globant.com>
2024-08-09 15:41:36 -0500

    Feat: Create multiply and divide methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,15 @@
 public class Calculator {
     public static void main(String[] args){
     }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) throws IllegalArgumentException {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
 }
